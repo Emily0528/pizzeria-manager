378483408384ddcb7ab11aa992ebec5d12af6b32
Urszula Lukaszek <lu.urszula@gmail.com>
2026-03-01 12:13:40 +0100

    Add Header, NavBar, Footer and wrap App in Container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import TablesList from './components/pages/TablesList';
 import TableDetails from './components/pages/TableDetails';
 import NotFound from './components/pages/NotFound';
+import Header from './components/views/Header';
+import Footer from './components/views/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TablesList />} />
-      <Route path="/table/:id" element={<TableDetails />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<TablesList />} />
+        <Route path="/table/:id" element={<TableDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
